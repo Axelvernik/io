d29788ead7a0e337af866a7117fe38d71ef7a975
Axelvernik <vernik.pl@gmail.com>
2024-02-26 10:21:44 +0200

    Update types.ts

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,6 +1,1 @@
-export type EndpointTypes = 'mainnet' | 'devnet' | 'localnet';
-declare global {
-  interface Window {
-    api?: any;
-  }
-}
+export type EndpointTypes = 'mainnet' | 'devnet' | 'localnet'
