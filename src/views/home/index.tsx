5ebccb68cfb0177e6d2da55ef2d5e0ae05df5a92
Axelvernik <vernik.pl@gmail.com>
2024-02-26 09:51:58 +0200

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -11,6 +11,9 @@ import pkg from '../../../package.json';
 
 // Store
 import useUserSOLBalanceStore from '../../stores/useUserSOLBalanceStore';
+import { Connection, PublicKey, Transaction } from '@solana/web3.js';
+import { notify } from 'utils/notifications';
+import { RPC_ENDPOINT } from './constant'
 
 export const HomeView: FC = ({ }) => {
   const wallet = useWallet();
