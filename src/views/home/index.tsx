91c3c1ac9d1bab923d269044281a6dc0d9e19764
Axelvernik <vernik.pl@gmail.com>
2024-03-27 12:58:27 +0200

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -31,7 +31,6 @@ export const HomeView: FC = ({ }) => {
       getUserSOLBalance(wallet.publicKey, connection)
     }
   }, [wallet.publicKey, connection, getUserSOLBalance])
-  import { useWallet } from "@solana/wallet-adapter-react"; // Or @jup-ag/wallet-adapter;
   const passthroughWalletContextState = useWallet();
 
   // To make sure passthrough wallet are synced
@@ -57,7 +56,6 @@ export const HomeView: FC = ({ }) => {
 
           </div>
 
-<div id="integrated-terminal"></div>
         <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
         <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
           Solana Next
