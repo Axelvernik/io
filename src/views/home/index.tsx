95bd1026033bac50870b0b26ede401474bb0f2ad
Axelvernik <vernik.pl@gmail.com>
2024-03-27 14:06:51 +0200

    8

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -51,6 +51,24 @@ export const HomeView: FC = ({ }) => {
 
           </div>
 
+        <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
+        <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
+
+        </h1>
+        </div>
+        <h4 className="md:w-full text-2x1 md:text-4xl text-center text-slate-300 my-2">
+
+        </h4>
+        <div className="relative group">
+          <div className="absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-indigo-500 rounded-lg blur opacity-40 animate-tilt"></div>
+          <div className="max-w-md mx-auto mockup-code bg-primary border-2 border-[#5252529f] p-6 px-10 my-2">
+            <pre data-prefix=">">
+              <code className="truncate">{`npx create-solana-dapp <dapp-name>`} </code>
+            </pre>
+          </div>
+        </div>
         
+      </div>
+    </div>
   );
 };
