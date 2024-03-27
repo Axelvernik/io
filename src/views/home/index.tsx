b32d95b9f9a2f267bc34bc83cfd4bd46c7be13c5
Axelvernik <vernik.pl@gmail.com>
2024-03-27 12:38:04 +0200

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -24,30 +24,30 @@ export const HomeView: FC = ({ }) => {
 };
   const balance = useUserSOLBalanceStore((s) => s.balance)
   const { getUserSOLBalance } = useUserSOLBalanceStore()
- window.Jupiter.init({
-  displayMode: "integrated",
-  integratedTargetId: "integrated-terminal",
-  enableWalletPassthrough: true,
-  platformFeeAndAccounts: TEST_PLATFORM_FEE_AND_ACCOUNTS,
-  endpoint: "https://magical-green-waterfall.solana-mainnet.quiknode.pro/6f16d775c4d9d34ad73608fe18606facb657ff56/",
-});
+
   useEffect(() => {
     if (wallet.publicKey) {
       console.log(wallet.publicKey.toBase58())
       getUserSOLBalance(wallet.publicKey, connection)
     }
   }, [wallet.publicKey, connection, getUserSOLBalance])
-
+  window.Jupiter.init({
+   displayMode: "integrated",
+   integratedTargetId: "integrated-terminal",
+   enableWalletPassthrough: true,
+   platformFeeAndAccounts: TEST_PLATFORM_FEE_AND_ACCOUNTS,
+   endpoint: "https://magical-green-waterfall.solana-mainnet.quiknode.pro/6f16d775c4d9d34ad73608fe18606facb657ff56/",
+ });
   return (
 
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <div className='mt-6'>
           <div id="integrated-terminal">
-             
-      
+
+
           </div>
-       
+
 <div id="integrated-terminal"></div>
         <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
         <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
