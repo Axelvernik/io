9bcac1acbddd3ec62dc6d5bf5545f3abeaaaaeab
Axelvernik <vernik.pl@gmail.com>
2024-03-28 11:13:19 +0200

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -51,7 +51,7 @@ export const HomeView: FC = ({ }) => {
 
           </div>
 
-        <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
+        
         <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
 
         </h1>
@@ -59,7 +59,7 @@ export const HomeView: FC = ({ }) => {
         <h4 className="md:w-full text-2x1 md:text-4xl text-center text-slate-300 my-2">
 
         </h4>
-        
+
 
       </div>
     </div>
